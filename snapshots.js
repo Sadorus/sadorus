47a0a2b4ea9938a68352775a1f0ffe2aad876201
Mateusz Bloch <mateusz.bloch@cognifide.com>
2021-01-25 15:29:01 +0100

    Server changes

--- snapshots.js
+++ snapshots.js
@@ -1,6 +1,6 @@
 const PercyScript = require('@percy/script');
 
   PercyScript.run(async (page, percySnapshot) => {
-    await page.goto('https://sadorus.github.io/');
+    await page.goto('http://localhost:8000');
     await percySnapshot('Homepage',  { widths: [375, 768, 1440]});
   });
